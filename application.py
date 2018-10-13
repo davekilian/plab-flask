4867bc37a4df16a31082530ab15c1e1dd6e18b16
Dave Kilian <dave.kilian@gmail.com>
2018-10-13 16:40:26 -0700

    Query comic info from table storage

--- application.py
+++ application.py
@@ -1,11 +1,30 @@
+
+from azure.cosmosdb.table.tableservice import TableService
+from azure.cosmosdb.table.models import Entity
+
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
+tablesvc = TableService(
+    account_name='peebleslab',
+    account_key='OxJE48VceGS4brzmrSwRwcUo3vB+q6cu+nt4I4elzlmEIcqxf28JLesGb8Qkb4060isc0MIESgcjP2H1HYIIKA==')
+
+def comicinfo(comic_id):
+    rowkey = str(comic_id).zfill(4)
+    entities = tablesvc.query_entities('comics', filter=f"PartitionKey eq 'comics' and RowKey eq '{rowkey}'")
+    return next(iter(entities))
+
+def maxcomicid():
+    rows = tablesvc.query_entities('comics', filter=f"PartitionKey eq 'global' and RowKey eq 'global'")
+    row = next(iter(rows))
+    return int(row.maxcomic)
 
 @app.route("/<int:comic_id>")
 def comic(comic_id):
-    return render_template('comic.html')
+    return "DEBUG: " + str(comicinfo(comic_id))
+    # return render_template('comic.html', info=comicinfo(comic_id))
 
 @app.route("/")
 def hello():
-    maxid = 100 # TODO: query storage to get max comic ID
-    return comic(maxid)
+    return comic(maxcomicid())
