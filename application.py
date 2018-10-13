ccade3010da639b6050eb5946c7e41f517c583ce
Dave Kilian <dave.kilian@gmail.com>
2018-10-13 16:13:30 -0700

    Render just an HTML dump of the live site

--- application.py
+++ application.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
+@app.route("/<int:comic_id>")
+def comic(comic_id):
+    return render_template('comic.html')
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    maxid = 100 # TODO: query storage to get max comic ID
+    return comic(maxid)
